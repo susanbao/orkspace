dc9afe853c0aa1bba44a5b662ebe7e1ed1374280
susanbao <susannju@163.com>
2023-07-06 17:28:39 +0000

    box ViT

--- install.sh
+++ install.sh
@@ -23,6 +23,8 @@ git clone https://github.com/NVIDIA/apex
 cd apex
 pip install -v --disable-pip-version-check --no-cache-dir --no-build-isolation --config-settings "--build-option=--cpp_ext" --config-settings "--build-option=--cuda_ext" ./
 pip install ml-collections
+pip install wandb
+pip install sockets
 echo "****************************************************************************************************************************************"
 echo "Complete installing lib for python"
 echo "****************************************************************************************************************************************"
